d3eb680fa324f1f15b0f1b96f9623c2e618accec
MehdiBenIa <mehdibenjelloun2026ia@gmail.com>
2025-12-11 12:13:56 +0100

    Add files via upload

---
+++ src/componentes/sobre-mi/sobre-mi.component.ts
@@ -0,0 +1,9 @@
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-mi',
+  templateUrl: './sobre-mi.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SobreMiComponent {}
